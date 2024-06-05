639124d0499d22929b3f1187716e0d811ae2805d
aridepai17 <advaithdepai26@gmail.com>
2024-06-05 14:11:19 +0530

    Light/Dark mode page essentials + visuals

---
+++ Light-Dark Mode Page/script.js
@@ -0,0 +1,11 @@
+$( ".inner-switch" ).on("click", function() {
+    if( $( "body" ).hasClass( "dark" )) {
+      $( "body" ).removeClass( "dark" );
+      $( ".inner-switch" ).text( "OFF" );
+    } 
+    else {
+      $( "body" ).addClass( "dark" );
+      $( ".inner-switch" ).text( "ON" );
+    }
+  });
+
